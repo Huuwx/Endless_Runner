4ff5664a68b6882cf145400fd679368dfdbd0b24
Huuwx <chumanhhuu2@gmail.com>
2025-02-16 18:10:40 +0700

    first step for ground turn

--- Assets/Scripts/GameController/GroundTileSpawner.cs
+++ Assets/Scripts/GameController/GroundTileSpawner.cs
@@ -8,7 +8,7 @@ public class GroundTileSpawner : MonoBehaviour
     public enum AXIS { XPositive, XNegative, ZPositive, ZNegative }
 
     public GameObject[] grounds;
-    public int initialSpawnCount = 5;
+    public int initialSpawnCount = 15;
     public float destoryZone = 300;
 
     [Space(10)]
@@ -21,8 +21,10 @@ public class GroundTileSpawner : MonoBehaviour
     public float maxSpeed = 25f;
 
 
-    public float groundSize = 30;
+    //public float groundSize = 30;
     GameObject lastGround;
+
+    public Vector3 nextSpawnPoint;
 
     [SerializeField] Transform groundSpawner;
 
@@ -39,61 +41,59 @@ public class GroundTileSpawner : MonoBehaviour
 
             ground.GetComponent<RunnerGroundTile>().spawner = this;
 
+            ground.transform.position = nextSpawnPoint;
+
             switch (axis)
             {
                 case AXIS.XPositive:
-                    ground.transform.localPosition = new Vector3(transform.position.x, 0, i * groundSize);
                     break;
 
                 case AXIS.XNegative:
-                    ground.transform.localPosition = new Vector3(transform.position.x, 0, i * groundSize);
                     break;
 
                 case AXIS.ZPositive:
-                    ground.transform.localPosition = new Vector3(transform.position.x, 0, i * groundSize);
                     moveDirection = new Vector3(0, 0, -1);
                     break;
 
                 case AXIS.ZNegative:
-                    ground.transform.localPosition = new Vector3(transform.position.x, 0, i * groundSize);
                     moveDirection = new Vector3(0, 0, -1);
                     break;
             }
 
 
             lastGround = ground;
+            nextSpawnPoint = ground.transform.GetChild(1).position;
         }
     }
 
     public void DestroyChunk(RunnerGroundTile thisGround)
     {
-        Vector3 newPos = lastGround.transform.position;
-        switch (axis)
-        {
-            case AXIS.XPositive:
-                newPos.x -= groundSize;
-                break;
+        nextSpawnPoint = lastGround.transform.GetChild(1).position;
+        //switch (axis)
+        //{
+        //    case AXIS.XPositive:
+        //        newPos.x -= groundSize;
+        //        break;
 
-            case AXIS.XNegative:
-                newPos.x -= groundSize;
-                break;
+        //    case AXIS.XNegative:
+        //        newPos.x -= groundSize;
+        //        break;
 
-            case AXIS.ZPositive:
-                newPos.z -= groundSize;
-                break;
+        //    case AXIS.ZPositive:
+        //        newPos.z -= groundSize;
+        //        break;
 
-            case AXIS.ZNegative:
-                newPos.z += groundSize;
-                break;
-        }
+        //    case AXIS.ZNegative:
+        //        newPos.z += groundSize;
+        //        break;
+        //}
 
         int groundIndex = Random.Range(0, grounds.Length);
 
-        GameObject ground = Instantiate(grounds[groundIndex], groundSpawner);
+        GameObject ground = Instantiate(grounds[groundIndex], nextSpawnPoint, Quaternion.identity, groundSpawner);
         ground.SetActive(true);
         ground.GetComponent<RunnerGroundTile>().spawner = this;
 
         lastGround = ground.gameObject;
-        lastGround.transform.position = newPos;
     }
 }
